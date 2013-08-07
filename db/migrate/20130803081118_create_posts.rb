bf05b694907719edc647287abf2540a008d4e463
n0-tactic <stimmeinferne@mail.ru>
2013-08-07 12:15:42 +0900

    Update 20130803081118_create_posts.rb

--- db/migrate/20130803081118_create_posts.rb
+++ db/migrate/20130803081118_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string  :title
-      t.string  :article
+      t.text  :article
       t.integer :need_symbols
       t.integer :category_id
       t.timestamps
