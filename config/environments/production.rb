25381bb4f297a20a862d8aa585ca6146c7dc5bb6
n0-tactic <stimmeinferne@mail.ru>
2013-08-07 11:00:06 +0900

    Production fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,12 +28,12 @@ Forum::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.precompile = ['highlight.pack.js', 'modernizr.js', 'admin.css', 'zenburn.css']
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  #config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
